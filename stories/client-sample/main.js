07a45c00559f5c3e99660ffc99dc5e714396cab9
1aoxomoxoa1 <atcohen60@gmail.com>
2023-07-19 17:56:38 +0300

    close btn

--- stories/client-sample/main.js
+++ stories/client-sample/main.js
@@ -1,5 +1,5 @@
 const headerText = `Greetings, human! Let's put a smile on that face! Tell us what you need and brace yourself for some fun!`
-const buttonsText = [`I'VE GOT AN AI-DEA`, `SPY ON COMPETITORS`, `LEMME SCROLL PLZ`];
+const buttonsText = [['CREATE', 'I know what I want'],[ 'EXPLORE', `I need some inspiration`], ['KEEP SCROLLING', 'Learn more about One AI']];
 
 function createHomePageMain() {
     // createChatUI({wrapper: document.body});
@@ -43,13 +43,20 @@ function createInterface(headerText, buttonsText) {
     bttnsContainer.className = 'chat-ui-controller-btns-ctr';
 
     for (const btnText of buttonsText) {
+        const [btnHeader, descText] = btnText;
+        console.log(btnHeader);
+        console.log(descText);
         const btn = document.createElement('div');
         btn.className = 'chat-ui-home-btns-wrapper';
 
-        const btnSpan = document.createElement('span');
-        btnSpan.className = 'chat-ui-btn';
-        btnSpan.innerHTML = btnText;
-        btn.append(btnSpan);
+        const headerSpan = document.createElement('span');
+        headerSpan.innerHTML = btnHeader;
+        headerSpan.style.color = '#00FFFF';
+        const descSpan = document.createElement('span');
+        descSpan.innerHTML = descText; 
+        descSpan.style.color = '#EDEDED'
+
+        btn.append(headerSpan, descSpan);
         bttnsContainer.append(btn);
     }
 
@@ -60,25 +67,41 @@ function createInterface(headerText, buttonsText) {
     return wrapper;
 }
 
+//cb for closing OneAgent
 function onCloseCallback() {
-
+    console.log('pooping');
+    document.querySelector('.sidebar-wrapper.fade-in').classList.toggle('fade-in');
+    document.querySelector('.sidebar-wrapper').classList.add('fade-out');
 }
 
+
+let chatUICreated = false; 
 function showBizGPTUI(event) {
     console.log('test');
     console.log(event);
 
-    createChatUI({ wrapper: document.querySelector('.chat-ui-wrapper'), onClose: () => { console.log("poop") } });
-
+    if(!chatUICreated){
+        createChatUI({ wrapper: document.querySelector('.chat-ui-wrapper'), onClose: onCloseCallback });
+        chatUICreated = true;
+    }
     //give style to the div surrounding the ONE AI chat
     const UIWrapper = document.querySelector('.chat-ui-wrapper').firstChild;
     UIWrapper.className = 'sidebar-wrapper'
     UIWrapper.classList.add('fade-in');
 
+    //then change the buttons when animation is finished 
+    setTimeout(replaceHomePageBtns, 450);
+}
 
-    //add event listener to the close button
+function replaceHomePageBtns() { 
+    console.log('pooping')
+    for(const child of document.querySelector('.interface-wrapper').children){
+        child.remove();
+    }
+
 
 }
+
 
 
 function attachEventListeners(raiseUICallback) {
